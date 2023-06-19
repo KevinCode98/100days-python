8b733c3f467475d36f2da68bff17602ef8bd2bda
KevinCode98 <kevincode98@gmail.com>
2023-06-18 20:12:26 -0600

    Day 2

---
+++ day2/Life-In-weeks.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+age_90 = 90 - int(age)
+
+days = age_90 * 365
+weeks = age_90 * 52
+months = age_90 * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
---
+++ day2/day-2-start.py
@@ -0,0 +1,18 @@
+# Data Types
+
+# String
+"Hello"[4]
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+
+123_456_678
+
+# Float
+3.14159
+
+# Boolean
+True
+False
