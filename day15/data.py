a37527ed8f31c7db73b7cd9d4878a66e916dacd2
KevinCode98 <kevincode98@gmail.com>
2023-07-03 18:08:47 -0600

    Day 15

---
+++ day15/data.py
@@ -0,0 +1,10 @@
+contacts = {
+    "James": {
+        "phone_number": 1234567890,
+        "email": "james@gmail.com"
+    },
+    "Jenny": {
+        "phone_number": 9876543211,
+        "email": "jenny@gmail.com"
+    }
+}
---
+++ day15/main.py
@@ -0,0 +1,12 @@
+from data import contacts
+
+jenny_email = contacts["Jenny"]["email"]
+
+
+def add(n1, n2):
+    total = n1 + n2
+    return total
+
+
+print(add(4, 5))
+add(3, 4)
