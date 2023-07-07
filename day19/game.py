5e10119ee0a58f09bbb1301f97dcf2f93987bfb8
KevinCode98 <kevincode98@gmail.com>
2023-07-06 20:35:15 -0600

    Day 19

---
+++ day19/game.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+all_turtles = []
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            winning_color = turtle.color()
+            is_race_on = False
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner! :D")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner! D:")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
